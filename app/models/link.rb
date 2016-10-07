fe1eed5df7a81943929c6ebd81845df7ba5efdc3
Mike Johns <mikejohns7@icloud.com>
2016-10-06 22:40:03 -0700

    Create basic model functionality for Twitter links

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,8 @@
 class Link < ApplicationRecord
+  def twitter
+    "https://twitter.com/intent/tweet?text=#{comment.gsub(/[\s]/, "%20")}&url=#{source}"
+  end
+  def facebook
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :links
-  root to: 'welcome#index'
+  root to: 'links#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
