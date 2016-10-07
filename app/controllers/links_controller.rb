e21f53e18edc2eaaa336b34c48c432fbfbca31a7
Mike Johns <mikejohns7@icloud.com>
2016-10-06 22:20:43 -0700

    Update Link controller

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,21 @@
 class LinksController < ApplicationController
+
+  def show
+    @link = Link.find(params[:id])
+  end
+
   def new
   end
 
   def create
-    render plain: params[:link].inspect
-    # render :new
+    @link = Link.new(link_params)
+    @link.save
+    redirect_to @link
+  end
+
+  private
+
+  def link_params
+    params.require(:link).permit(:source, :comment)
   end
 end
