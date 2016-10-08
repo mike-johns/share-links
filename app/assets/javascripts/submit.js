a1bb90ab3c332219885e821827181b419e2cc7ec
Mike Johns <mikejohns7@icloud.com>
2016-10-07 17:27:46 -0700

    Fix jQuery listeners to fire on turbolinks reloads

--- app/assets/javascripts/submit.js
+++ app/assets/javascripts/submit.js
@@ -1,16 +1,20 @@
-jQuery(function() {
+document.addEventListener("turbolinks:load", function() {
   $field = $('#link_comment');
   $text = $('#comment-length');
   var length, remaining;
 
+  $field.keypress(function() {
+    length = $field.val().length;
+    update(length);
+  });
+
   $field.change(function(e) {
     length = $field.val().length;
     update(length);
-    console.log(length);
   });
 
   function update(length) {
-    remaining = 116 - length;
+    remaining = 115 - length;
     $text.text(remaining + ' Characters Left');
   };
 });
